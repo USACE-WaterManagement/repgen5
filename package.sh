5677ec81068b9e32ce6616f306152c9db02fd1de
Mike Neilson <michael.a.neilson@usace.army.mil>
2021-02-12 11:22:51 -0800

    error in build script.

--- package.sh
+++ package.sh
@@ -1,5 +1,6 @@
 mkdir build
 zip -r build/repgen.zip __main__.py
 zip -r build/repgen.zip repgen
+zip -r build/repgen.zip LICENSE.md
 echo '#!/bin/env python3' | cat - build/repgen.zip > build/repgen
 chmod +x build/repgen
