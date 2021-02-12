27dc8d43b6486240e05cc77e000dafa1dc244619
Mike Neilson <michael.a.neilson@usace.army.mil>
2021-02-12 09:58:32 -0800

    forgot timezone on other test.

--- tests/test_value.py
+++ tests/test_value.py
@@ -22,7 +22,7 @@ def test_gents_generator():
         return data.thedata[data.index-1]
     data.index = 0
     data.thedata = range(9)
-    t_end = datetime.datetime.now().replace(minute=0,second=0,microsecond=0)
+    t_end = datetime.datetime.now().replace(minute=0,second=0,microsecond=0,tzinfo=TZ("UTC"))
     t_start = t_end-datetime.timedelta(hours=2)
     v = Value(dbtype="gents",value = data,tz="PST8PDT", start=t_start,end=t_end, interval=datetime.timedelta(minutes=15), picture="%0.02f")
     assert len( v.values ) == 9    
