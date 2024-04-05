2606a64646ae6e2a20fd03d53423e068d1af6649
Daniel Osborne <Daniel.T.Osborne@usace.army.mil>
2024-04-05 11:41:41 -0700

    Improve packaging to build and include pyc.

--- package.sh
+++ package.sh
@@ -1,6 +1,11 @@
-mkdir build
+#!/bin/sh
+dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+cd $dir
+mkdir build 2>/dev/null
+python -c "import compileall; compileall.compile_dir('.', quiet=1)"
 zip -r build/repgen.zip __main__.py
-zip -r build/repgen.zip repgen -x *.pyc
+zip -r build/repgen.zip __pycache__
+zip -r build/repgen.zip repgen
 zip -r build/repgen.zip LICENSE.md
 echo '#!/usr/bin/env python3' | cat - build/repgen.zip > build/repgen
 chmod +x build/repgen
