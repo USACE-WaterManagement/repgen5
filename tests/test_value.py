c577bc760839ec9e6f19427141e93e475fbb2e01
Daniel Osborne <Daniel.T.Osborne@usace.army.mil>
2024-09-13 14:08:36 -0700

    Add test.

--- tests/test_value.py
+++ tests/test_value.py
@@ -33,3 +33,21 @@ def test_gents_generator():
     assert v.values[3][1] == 3
     assert v.values[4][1] == 4
     assert v.values[8][1] == 8
+
+def test_nomiss():
+    """
+    This tests the NOMISS property. When Value.missing is set to NOMISS,
+    attempting to retrieve a value that doesn't exist will cause repgen to return the most recent
+    value found that's earlier than the requested timestamp.
+    """
+    def data():
+        data.index+=1
+        return data.thedata[data.index-1]
+    data.index = 0
+    data.thedata = [0, 1, 2, 3, 4, 5, 6, None, None]
+    t_end = datetime.datetime.now().replace(minute=0,second=0,microsecond=0,tzinfo=TZ("UTC"))
+    t_start = t_end-datetime.timedelta(hours=2)
+    v = Value(dbtype="gents", value = data, tz="PST8PDT", missing="NOMISS", start=t_start,end=t_end, interval=datetime.timedelta(minutes=15), picture="%0.02f")
+    assert len( v.values ) == 9
+    assert v.pop() == "0.00"
+    assert v[t_end].value == 6
